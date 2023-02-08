96444a2718480fca009bf4083418e11ba8f13ed3
jie <jie@jies-MacBook-Pro.local>
2023-02-08 14:47:28 +0200

    Update key

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const {Configuration, OpenAIApi} = require("openai");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.json());
+
+const configuration = new Configuration({apiKey: "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"});
+const openai = new OpenAIApi(configuration);
+
+
+app.post("/api/question", async (req, res) => {
+    const { prompt } = req.body;
+    console.info(prompt)
+    console.info('model: "text-davinci-003"')
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0
+    });
+    console.info(response.data)
+    res.json(response.data)
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
